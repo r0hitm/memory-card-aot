9d3c64cb703c241f72ae1511fdc0d9c8b247937f
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-03-31 02:05:40 +0530

    basic front-end code ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentScore, setCurrentScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+
+    function updateScore() {
+        setCurrentScore(currentScore + 1);
+        if (currentScore + 1 > bestScore) {
+            setBestScore(currentScore + 1);
+        }
+    }
+
+    return (
+        <div className="App">
+            <h1>
+                進撃の巨人
+                <br />
+                Memory Card Game
+            </h1>
+            <p>
+                <button class="help">How to Play?</button>
+            </p>
+            <div class="score-board">
+                <div class="current">
+                    <strong>Current Score</strong>: <span>{currentScore}</span>
+                </div>
+                <div class="best">
+                    <strong>Best Score</strong>: <span>{bestScore}</span>
+                </div>
+            </div>
+            <div class="cards"></div>
+            <footer>
+                <p>
+                    Made by <a href="https://github.com/r0hitm">Rohit Mehta</a>
+                </p>
+                |
+                <p>
+                    View on{" "}
+                    <a href="https://github.com/r0hitm/memory-card-aot">
+                        GitHub
+                    </a>
+                </p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;

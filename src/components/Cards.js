c1a6a9f137701070ef97a445c5ca3e512464bb9f
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-04-01 11:02:55 +0530

    Almost done, optimizing and refactoring

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 // Get all images from the img folder
 const images = require.context("../img", true);
@@ -6,44 +6,88 @@ const imagePaths = images.keys().map(path => images(path));
 // console.log(imagePaths);
 
 export default function Cards({ updateScore, resetScore }) {
-    // Array of booleans to keep track of which cards have been clicked
-    const [cards, setCards] = useState(Array(imagePaths.length).fill(false));
+    // Array of objects with a clicked property and an imgPathIndex property
+    const [cards, setCards] = useState(
+        imagePaths.map((_, i) => ({
+            clicked: false,
+            imgPathIndex: i,
+        }))
+    );
 
-    function handleClick(i) {
-        console.log("Card clicked"); // DEBUG
-        if (cards[i]) {
-            resetScore();
-            setCards(Array(imagePaths.length).fill(false));
-        } else {
-            updateScore();
-            setCards([...cards.slice(0, i), true, ...cards.slice(i + 1)]);
+    // function handleClick(i) {
+    //     if (cards[i].clicked) {
+    //         // console.log("You clicked on a card that was already clicked!");  // DEBUG
+    //         resetScore();
+    //         const newCards = cards;
+    //         newCards.forEach(card => (card.clicked = false));
+    //         console.assert(cards.every(card => !card.clicked));
+    //         setCards(newCards);
+    //     } else {
+    //         // console.log("You clicked on a new card!");   // DEBUG
+    //         updateScore();
+    //         const newCards = cards;
+    //         newCards[i].clicked = true;
+    //         setCards(newCards);
+    //         console.assert(cards[i].clicked);
+    //     }
+
+    //     shuffleCards(cards);
+    // }
+
+    const handleClick = useCallback(
+        i => {
+            const cardsCopy = [...cards];
+            if (cards[i].clicked) {
+                // console.log("You clicked on a card that was already clicked!");  // DEBUG
+                resetScore();
+                cardsCopy.forEach(card => (card.clicked = false));
+                setCards(cardsCopy);
+
+                console.assert(cards.every(card => !card.clicked)); // DEBUG
+            } else {
+                // console.log("You clicked on a new card!");   // DEBUG
+                updateScore();
+                cardsCopy[i].clicked = true;
+                setCards(cardsCopy);
+
+                console.assert(cards[i].clicked); // DEBUG
+            }
+
+            shuffleCards(cards);
+        },
+        [cards, updateScore, resetScore]
+    );
+
+    function shuffleCards(cardsCopy) {
+        // Fisher-Yates shuffle
+        for (let i = cardsCopy.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [cardsCopy[i], cardsCopy[j]] = [cardsCopy[j], cardsCopy[i]];
         }
+        setCards(cardsCopy);
     }
 
     return (
         <div className="cards">
-            {cards.map((clicked, i) => (
+            {cards.map((_, i) => (
                 <Card
                     key={i}
                     onClick={() => handleClick(i)}
-                    imgPath={imagePaths[i]}
-                    // clickState={clicked}
+                    // imgPath={imagePaths[i]}
+                    cardImg={imagePaths[cards[i].imgPathIndex]}
                 />
             ))}
         </div>
     );
 }
 
-function Card({ onClick, imgPath, clickState }) {
-    // const [clicked, setClicked] = useState(clickState);
-    // const [imageURL, setImageURL] = useState(img);
-
+function Card({ onClick, cardImg }) {
     return (
         <div
             className="card"
             onClick={onClick}
             style={{
-                backgroundImage: `url(${imgPath})`,
+                backgroundImage: `url(${cardImg})`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
             }}
